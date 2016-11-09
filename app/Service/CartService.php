57b23922256e6fcf047a0da9c09edfd3864dfa11
minion <minion@minion-2.local>
2016-11-09 10:31:16 +0900

    カート処理をクラス移行。

--- app/Service/CartService.php
+++ app/Service/CartService.php
@@ -18,8 +18,86 @@
 
 class CartService
 {
-    /*      */
-    public function addItem($id){
-        
+    /**
+     *
+     *      カート一覧表示
+     *
+     */
+    public function showCart(){
+        $items = session()->get("items",[]); //セッションデータを取得、nullの場合は空の配列
+        $itemCount = []; // id => num
+        $itemMap = []; // id => $item
+        $totalSum = 0;  //合計金額
+        //多分、商品情報を、itemMapに入れて、totalSumに合計金額を格納する処理
+        foreach ($items as  $item) {
+            if(isset($itemCount[$item->id])){
+                $itemCount[$item->id] = $itemCount[$item->id] + 1;  //カウントを追加
+            }else{
+                $itemCount[$item->id] = 1;
+                $itemMap[$item->id] = $item; //多分、商品情報を入れる。
+            }
+            //合計金額をtotalSumに格納
+            $totalSum = $totalSum + $item->price;
+        }
+        //変数を返す。　受け取り先で、list(変数名,変数名)=X.showCart()で展開する。
+        return [$items,$itemCount,$itemMap,$totalSum];
     }
+
+
+
+
+    /**
+     *
+     *      カートに追加する処理
+     *
+     */
+    public function addItem($id,$num){
+        //idが一致するものをテーブルから検索、取得
+            $item = DB::table('pizza')->where('id', $id)->first();
+        //既にカートに商品が入っていたらそれを$itemsに読み込む
+            $items = session()->get("items",[]); //セッションデータを取得、nullの場合は空の配列
+        //個数の処理
+            if($num > 0){
+                for($i=0;$i<$num;$i++){
+                    $items[] = $item; // 取得したデータにオブジェクトを保存
+                }
+            }
+        //セッションに商品を追加する
+             session()->put("items", $items);
+    }
+
+
+
+    /**
+     *
+     *      カートを全削除
+     *
+     */
+    public function clear(){
+        session()->flush(); //sessionの全データを削除
+    }
+
+
+
+    /**
+     *
+     *      カートの一部商品を削除
+     *
+     */
+    public function removeItem($itemId){
+        $items = session()->get("items",[]); //セッションデータを取得、nullの場合は空の配列
+        foreach ($items as $index => $item) {
+            if($item->id == $itemId){
+                session()->forget("items.$index"); //sessionから選んだ商品を削除。例えば$items[0]の削除は items.0 と指定できる。
+            }
+        }
+    }
+
+
+
+
 }
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -46,25 +46,18 @@ Route::get('/detail', function (Request $request) {
   });
 
 
+
+
 // カートに追加
   Route::post('/cart', function(Request $request){
       //idを取得し、$idに商品idを、$itemに商品データを格納
         $id = $request->get("id"); //idを取得
-        $item = DB::table('pizza')->where('id', $id)->first(); //idが一致するものをテーブルから検索、取得
-      //既にカートに商品が入っていたらそれを$itemsに読み込む
-        $items = session()->get("items",[]); //セッションデータを取得、nullの場合は空の配列
-
       //個数の処理
-          $num = 0;
-          $num = $request->get("num");  //個数を取得
-          if($num > 0){
-            for($i=0;$i<$num;$i++){
-              $items[] = $item; // 取得したデータにオブジェクトを保存
-            }
-          }
-      //セッションに商品を追加する
-         session()->put("items", $items);
-
+        $num = 0;   //0で初期化
+        $num = $request->get("num");  //個数を取得
+      //クラス読み込み
+        $cart = new \App\Service\CartService();
+        $cart->addItem($id,$num);
       return redirect("cart"); //カートのページへリダイレクト
   });
 
@@ -73,22 +66,11 @@ Route::get('/detail', function (Request $request) {
 
   // カートの中を一覧表示
   Route::get('/cart', function(){
-      $items = session()->get("items",[]); //セッションデータを取得、nullの場合は空の配列
-      $itemCount = []; // id => num
-      $itemMap = []; // id => $item
-      $totalSum = 0;  //合計金額
+      //クラス読み込み　オブジェクト生成
+         $cart = new \App\Service\CartService();
+      //list(変数名,変数名) = 配列とかにすると、配列の中身を受け渡すことができる
+         list($items,$itemCount,$itemMap,$totalSum) = $cart->showCart();
 
-      //多分、商品情報を、itemMapに入れて、totalSumに合計金額を格納する処理
-      foreach ($items as  $item) {
-        if(isset($itemCount[$item->id])){
-          $itemCount[$item->id] = $itemCount[$item->id] + 1;  //カウントを追加
-        }else{
-          $itemCount[$item->id] = 1;
-          $itemMap[$item->id] = $item; //多分、商品情報を入れる。
-        }
-        //合計金額をtotalSumに格納
-          $totalSum = $totalSum + $item->price;
-      }
       return view("pizza_ec.cart", [ //データを渡してビューを表示
           "items" => $items ,
           "count" => $itemCount ,
@@ -102,7 +84,10 @@ Route::get('/detail', function (Request $request) {
 
   // カートを空にする
   Route::get('/delete/all', function(){
-      session()->flush(); //sessionの全データを削除
+      //クラス読み込み　オブジェクト生成
+        $cart = new \App\Service\CartService();
+        $cart->clear();
+
       return redirect("/cart"); //カートのページへリダイレクト
   });
 
@@ -111,14 +96,10 @@ Route::get('/detail', function (Request $request) {
 
   // カートの一部商品を削除する
   Route::get('/delete', function(Request $request){
-    $itemId = $request->get("id"); //削除した商品のindexを取得。 delete?index=Xの値が入る
-    $items = session()->get("items",[]); //セッションデータを取得、nullの場合は空の配列
-      foreach ($items as $index => $item) {
-          if($item->id == $itemId){
-              session()->forget("items.$index"); //sessionから選んだ商品を削除。例えば$items[0]の削除は items.0 と指定できる。
-          }
-      }
-//      $data = $request->session()->all();
-//      dd($data,$index);
+      //itemIdに、削除された商品のidを入れる。
+        $itemId = $request->get("id"); //削除した商品のidを取得。 delete?id=Xの値が入る
+      //クラス読み込み　オブジェクト生成
+        $cart = new \App\Service\CartService();
+        $cart->removeItem($itemId);
     return redirect("/cart");
 });
